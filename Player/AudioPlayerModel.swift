a82a0141cf8d7149f0cfed662f348fd383df3c7d
Shunya Takahashi <s-takahashi.sra@gmo-click.com>
2017-11-10 17:00:33 +0900

    再生周りの処理を仮追加

--- Player/AudioPlayerModel.swift
+++ Player/AudioPlayerModel.swift
@@ -17,18 +17,29 @@ class AudioPlayerModel {
     
     private var playQueue: [AudioItem] = []
     
+    private(set) var index: Int = 0
+    private(set) var playing: AudioItem?
     var isShuffle: Bool = false
     var isLoop: Bool = false
+    
+    let queueModel = PlayQueueModel.instance
     
     private var player: AVAudioPlayer?
     
     func play() {
-        player?.pause()
-        //TODO
+        if let player = player {
+            playRevert(player: player)
+        } else {
+            playNew()
+        }
+    }
+    
+    private func playNew() {
         AudioItemManager.shared.load()
         playQueue = AudioItemManager.shared.items
         if let url = playQueue.first?.path.fileUrl {
             do {
+                playing = playQueue.first
                 player = try AVAudioPlayer(contentsOf: url)
                 player?.prepareToPlay()
                 player?.play()
@@ -37,6 +48,10 @@ class AudioPlayerModel {
                 return
             }
         }
+    }
+    
+    private func playRevert(player: AVAudioPlayer) {
+        player.play()
     }
     
     func pause() {
@@ -51,8 +66,39 @@ class AudioPlayerModel {
         //TODO
     }
     
-    func shuffle() {
-        //TODO
+    func shufflePlay() {
+        queueModel.shuffle()
     }
+    
+}
 
+class PlayQueueModel {
+    static let instance = PlayQueueModel()
+    private init() { }
+    
+    private(set) var queue: [AudioItem] = []
+    private(set) var index: Int = 0
+    
+    func current() -> AudioItem? {
+        return queue.dropFirst(index).first
+    }
+    
+    func toNext() -> AudioItem? {
+        index = min(index + 1, queue.count - 1)
+        return current()
+    }
+    
+    func toPrevious() -> AudioItem? {
+        index = max(0, index - 1)
+        return current()
+    }
+    
+    func shuffle() {
+        var tmp = queue.shuffled
+        let playing = queue.dropFirst(index).first
+        if let playing = playing, let index = tmp.map({ $0.path }).index(of: playing.path) { //なぜかmapの()を省略するとエラーになる
+            tmp.move(from: index, to: 0)
+        }
+    }
+    
 }
